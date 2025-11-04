a97174b3de0dd368560464160ce8fc8d7ecc1eaf
Soban Saeed <sobansaeed123@gmail.com>
2025-11-04 12:01:33 +0000

    Coming sooon text

--- src/app/page.js
+++ src/app/page.js
@@ -11,12 +11,9 @@ import Footer from '../components/Footer/Footer';
 import DesktopHero from '../components/DesktopHero/DesktopHero'
 import MobileHero from '../components/MobileHero/MobileHero'
 import Guide from '../components/Guide/Guide'
-import HeroNew from '../components/HeroNew/HeroNew';
 import HeroBottom from '../components/HeroBottom/HeroBottom';
-import TextType from '../components/TypingText/TypingText';
+import ScrollVelocity from '../components/ScrollVelocity/ScrollVelocity';
 
-
-const defaultContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo at fuga cum dolore pariatur culpa, iste cupiditate delectus soluta natus aut sunt, provident autem eveniet quisquam similique minima dolor voluptatem!"
 export default function Home() {
   return (
 
@@ -36,17 +33,12 @@ export default function Home() {
         className="custom-text-black"
       /> */}
 
-      <section className="flex justify-center items-center py-12 px-4 bg-[#f1f1f1]">
-        <TextType
-          text={["Something amazing is coming your way!", "We’re finalizing development — launching soon."]}
-          typingSpeed={50}
-          pauseDuration={500}
-          showCursor={true}
-          initialDelay={1000}
-          cursorCharacter="_"
-          className="text-4xl md:text-5xl lg:text-6xl font-semibold text-center font-narkiss-hadash bg-gradient-to-r from-primary-light to-primary-dark bg-clip-text text-transparent"
+      <div style={{ backgroundColor: '#f2f2f2' }}>
+        <ScrollVelocity
+          texts={['Something amazing is coming your way!', 'We\'re finalizing development — launching soon.']}
+          velocity={70}
         />
-      </section>
+      </div>
 
       <CardStackScroll />
       <Guide />
